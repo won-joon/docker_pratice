0a8b91757e86c1a30ce544879cfe220911a28b56
WonJoon <seowj@gmail.com>
2023-07-03 19:33:46 +0900

    docker

---
+++ docker_practice/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Docker!');
+});
+
+app.listen(8080, () => console.log('Server is running'));
